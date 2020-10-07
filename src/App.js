19b173a4a3a81c9c56f094de13a40b5b410879ad
Yvette <nchombuayvta@gmil.com>
2020-10-08 01:53:23 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import ReactGA from 'react-ga';
 import $ from 'jquery';
 import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import About from './Components/About';
-import Resume from './Components/Resume';
-import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
-import Portfolio from './Components/Portfolio';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+import Testimonials from './components/Testimonials';
+import Portfolio from './components/Portfolio';
 
 class App extends Component {
 
@@ -23,7 +23,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'/resumeData.json',
+      url:'/resume.json',
       dataType:'json',
       cache: false,
       success: function(data){
